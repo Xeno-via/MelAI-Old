86d5a7f79ce890ba3bf58c34537e047f3e685edf
= <figgerthedigger@gmail.com>
2022-05-20 23:08:43 +0100

    hehe funny menny changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Finn_\Documents\Diss Stuff\MelAI\venv
+set VIRTUAL_ENV=C:\Users\Finn_\Documents\MelAI\MelAI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
